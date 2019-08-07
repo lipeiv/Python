8cd1fdd8bce8d1747b7175492dfdd47c590d67a9
lipeiv <lipeiv@qq.com>
2019-08-07 09:02:44 +0800

    Python

--- Hello/he.py
+++ Hello/he.py
@@ -1,1 +1,10 @@
- print("Hello git") 
+from redis import *
+
+if __name__ == "__main__":
+    try:
+        sr = StrictRedis(host='localhost', port=6379, db=0)
+        result = sr.get("a")
+        print(result)
+    except Exception as e:
+        print(e)
+
