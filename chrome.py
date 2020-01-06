a5c5ee62f27818a5a5476397c37e680422283240
lipeiv <lipeiv@qq.com>
2020-01-07 00:02:47 +0800

    python

--- chrome.py
+++ chrome.py
@@ -1,4 +1,10 @@
-from selenium import webdriver
-browser = webdriver.Chrome()
-url = "https://www.baidu.com"
-browser.get(url)
+import csv
+import pymongo
+import redis
+
+from redis import StrictRedis
+
+with open('data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['id', 'name', 'age'])
+    writer.writerow(['1','lipeiv','18'])
