817f3670976f2119e1417a07ee770e2cbd4f1f84
lipeiv <lipeiv@qq.com>
2020-03-26 19:08:08 +0900

    'mod'

--- gecko.py
+++ gecko.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 browser = webdriver.Chrome()
 
-browser.get("http://www.lipeipei.com.cn")
-print(browser.page_source)
+
+print(browser.find_elements_by_id("percent1"))
 browser.close()
