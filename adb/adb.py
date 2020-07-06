7df17d1ca74bd1cc08c71140bd5df2e0cbd0a043
lipeiv <lipeiv@qq.com>
2020-07-07 00:55:11 +0800

    mod

--- adb/adb.py
+++ adb/adb.py
@@ -6,8 +6,10 @@ def screen():
 	os.system("adb shell screencap /sdcard/0.png")
 	time.sleep(1)
 	os.system("adb pull /sdcard/0.png .")
-
 	image = Image.open("0.png")
 	image.show()
+
+#for i in range(10):
+#	screen()
 
 screen()
