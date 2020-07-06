3aba732eb40ecc49415241397cca55aa5cd767d4
lipeiv <lipeiv@qq.com>
2020-07-07 00:01:12 +0800

    add

--- adb/adb.py
+++ adb/adb.py
@@ -2,9 +2,12 @@ import os
 import time
 from PIL import Image
 
-os.system("adb shell screencap /sdcard/0.png")
-time.sleep(1)
-os.system("adb pull /sdcard/0.png .")
+def screen():
+	os.system("adb shell screencap /sdcard/0.png")
+	time.sleep(1)
+	os.system("adb pull /sdcard/0.png .")
 
-image = Image.open("0.png")
-image.show()
+	image = Image.open("0.png")
+	image.show()
+
+screen()
